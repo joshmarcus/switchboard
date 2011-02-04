0d9915f6d4becbcc1a4fcb3a5af5ecb05d4fb64b
k clair <kclair@localhost.localdomain>
2011-02-03 22:57:52 -0500

    change deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 set :application, "Switchboard"
-set :repository,  "ssh://gitosis@durga.serve.com:2020/~/repositories/mmp-sms.git"
+set :repository,  "git@github.com:switchboard/switchboard.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :branch, "deploy"
 
-role :web, "mmp.vpscustomer.com"                          # Your HTTP server, Apache/etc
-role :app, "mmp.vpscustomer.com"                          # This may be the same as your `Web` server
-role :db,  "mmp.vpscustomer.com", :primary => true # This is where Rails migrations will run
+role :web, "69.164.216.141"                          # Your HTTP server, Apache/etc
+role :app, "69.164.216.141"                          # This may be the same as your `Web` server
+role :db,  "69.164.216.141", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 set :user, "switchboard"
